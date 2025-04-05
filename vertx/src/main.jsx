59b1d96d71c63a1d02eeacda0f165fbd6e408a42
Akshara <amuppall06@gmail.com>
2025-04-05 14:25:58 +0530

    Height

--- vertx/src/main.jsx
+++ vertx/src/main.jsx
@@ -1,20 +1,20 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
+import SideBar from "./components/SideBar/index.jsx";
 
-import SideBar from './components/SideBar/index.jsx';
 import TopBar from "./components/TopBar/index.jsx";
-import DashBoard from './pages/Dashboard/index.jsx';
+import DashBoard from "./pages/DashBoard/index.jsx";
 
-import './index.css';
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <TopBar/>
-    
-    <div class="flex flex-row">
+    <TopBar />
+
+    <div class="flex flex-row h-full">
       <SideBar />
-      <DashBoard/>
+      <DashBoard />
     </div>
   </StrictMode>
 );
