14b405d42541e037d745659b397dff8b0e699c35
Santhosh Siva <santhosh@Santhoshs-MacBook-Pro.local>
2025-04-05 13:41:06 +0530

    fix: done

--- vertx/src/main.jsx
+++ vertx/src/main.jsx
@@ -3,11 +3,10 @@ import { createRoot } from 'react-dom/client';
 
 
 import SideBar from './components/SideBar/index.jsx';
-
-
 import TopBar from "./components/TopBar/index.jsx";
 import DashBoard from './pages/Dashboard/index.jsx';
 
+import './index.css';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
